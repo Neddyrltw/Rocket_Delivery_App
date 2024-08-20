4260dad6aea9e7e20d87949721b78b73cc30c6b2
Nicholas Eddy <nicholas.g.eddy@gmail.com>
2024-08-20 14:27:14 -0400

    login page buildout started

--- client/App.js
+++ client/App.js
@@ -1,14 +1,41 @@
-import React from 'react';
-import { View, Text, Image, SafeAreaView, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View,
+  Text,
+  TextInput,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  TouchableOpacity } from 'react-native';
 
 const App = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  
   return (
     <SafeAreaView style={styles.wrapper}>
       <View style={styles.container}>
         <Image></Image>
-        <Text>Rocket Delivery</Text>
-        <Text>Welcome back!</Text>
+        <Text style={styles.welcome}>Welcome back!</Text>
         <Text>Login to begin</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={setEmail}
+          value={email}
+          placeholder='Enter your primary email here'
+          autoCorrect={false}
+          keyboardType='email-address'
+        />
+      <View style={styles.passwordContainer}>
+        <TextInput
+          style={styles.input}
+          onChangeText={setPassword}
+          value={password}
+          placeholder='●●●●●●●●'
+        />
+      </View>
+   
+        
+      
     </View>
     </SafeAreaView>
   )
@@ -19,11 +46,31 @@ const styles = StyleSheet.create({
     flex: 1
   },
   container: {
-    backgroundColor: 'pink',
+    backgroundColor: '#FFFFFF',
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  passwordContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%'
+  },
+  input: {
+    height: 40,
+    width: '60%',
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 5,
+  },
+  toggleButton: {
+    marginLeft: -50,
+    padding: 10
+  },
+  welcome: {
+    fontWeight: 'bold'
   }
- 
 })
 export default App;
